2ee3de6c178f5be9aa3baea995e60e6cadec0367
Zehra Çelik <zehra.celikk2002@gmail.com>
2023-10-08 22:45:57 +0300

    random choice picker completed

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -10,6 +10,9 @@ textarea.addEventListener('keyup', (e) => {
         setTimeout(() => {
             e.target.value = ''
         }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input) {
@@ -26,3 +29,39 @@ function createTags(input) {
 }
 
 
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+    } , times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
